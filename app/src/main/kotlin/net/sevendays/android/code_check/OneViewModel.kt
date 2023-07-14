55c3de82407ed54fe31a7ef27ecce652e7ba70e0
Stefan Teau <50839452+StefanTeau7@users.noreply.github.com>
2023-07-14 13:48:23 +0300

    Memory leak for search result, crash fix for 'item' nullability, JSON parsing fix

--- app/src/main/kotlin/net/sevendays/android/code_check/OneViewModel.kt
+++ app/src/main/kotlin/net/sevendays/android/code_check/OneViewModel.kt
@@ -6,54 +6,53 @@ package net.sevendays.android.code_check
 import android.content.Context
 import android.os.Parcelable
 import androidx.lifecycle.ViewModel
+import coil.request.ErrorResult
+import coil.request.ImageRequest
+import coil.request.SuccessResult
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.engine.android.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
-import net.sevendays.android.code_check.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import net.sevendays.android.code_check.TopActivity.Companion.lastSearchDate
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
 import kotlinx.parcelize.Parcelize
+import net.sevendays.android.code_check.R
 import org.json.JSONObject
+import java.text.SimpleDateFormat
 import java.util.*
 
-/**
- * Use with TwoFragment
- */
 class OneViewModel(
-    val context: Context
+    private val context: Context
 ) : ViewModel() {
 
-    // Search results
-    fun searchResults(inputText: String): List<item> = runBlocking {
-        val client = HttpClient(Android)
+    private val httpClient = HttpClient(Android)
 
-        return@runBlocking GlobalScope.async {
-            val response: HttpResponse = client?.get("https://api.github.com/search/repositories") {
+    suspend fun searchResults(inputText: String): List<item> {
+        return withContext(Dispatchers.IO) {
+            val response: HttpResponse = httpClient.get("https://api.github.com/search/repositories") {
                 header("Accept", "application/vnd.github.v3+json")
                 parameter("q", inputText)
             }
 
             val jsonBody = JSONObject(response.receive<String>())
-
-            val jsonItems = jsonBody.optJSONArray("items")!!
+            val jsonItems = jsonBody.optJSONArray("items") ?: return@withContext emptyList()
 
             val items = mutableListOf<item>()
 
-            /**
-             * Loop for the number of items
-             */
             for (i in 0 until jsonItems.length()) {
-                val jsonItem = jsonItems.optJSONObject(i)!!
+                val jsonItem = jsonItems.optJSONObject(i) ?: continue
+
                 val name = jsonItem.optString("full_name")
-                val ownerIconUrl = jsonItem.optJSONObject("owner")!!.optString("avatar_url")
+                val ownerIconUrl = jsonItem.optJSONObject("owner")?.optString("avatar_url")
                 val language = jsonItem.optString("language")
                 val stargazersCount = jsonItem.optLong("stargazers_count")
                 val watchersCount = jsonItem.optLong("watchers_count")
-                val forksCount = jsonItem.optLong("forks_conut")
+                val forksCount = jsonItem.optLong("forks_count")
                 val openIssuesCount = jsonItem.optLong("open_issues_count")
 
                 items.add(
@@ -71,18 +70,18 @@ class OneViewModel(
 
             lastSearchDate = Date()
 
-            return@async items.toList()
-        }.await()
+            return@withContext items
+        }
     }
 }
 
 @Parcelize
 data class item(
     val name: String,
-    val ownerIconUrl: String,
+    val ownerIconUrl: String?,
     val language: String,
     val stargazersCount: Long,
     val watchersCount: Long,
     val forksCount: Long,
-    val openIssuesCount: Long,
+    val openIssuesCount: Long
 ) : Parcelable
